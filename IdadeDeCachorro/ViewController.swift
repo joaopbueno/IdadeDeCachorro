f26abe87b7c2498e4544920c5a24c630565b9534
joaopbueno <mrjoao16@gmail.com>
2024-10-17 10:16:37 -0300

    Initial Commit

---
+++ IdadeDeCachorro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IdadeDeCachorro
+//
+//  Created by João Bueno on 17/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
